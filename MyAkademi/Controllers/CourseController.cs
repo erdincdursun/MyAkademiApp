b651966fb464f02bb07a5acf18aa76d894f8be7b
Erdinç Dursun <113687007+erdincdursun@users.noreply.github.com>
2023-05-29 09:13:44 +0300

    MyAkademiApp

---
+++ MyAkademi/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAkademi.Models;
+
+namespace MyAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var candidates = Repository.Applications;
+            return View(candidates);
+        }
+
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm]Candidate candidate)
+        {
+            if(Repository.Applications.Any(a => a.Email.Equals(candidate.Email)))
+            {
+                ModelState.AddModelError("", "There is already application for you.");
+            }
+            if (ModelState.IsValid)
+            {
+                Repository.Add(candidate);
+                return View("Feedback", candidate);
+            }
+            return View();
+        }
+    }
+}
